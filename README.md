57daeba4051efefbec5827a7a7d9a8083e9e078e
Mark Watson <markw@markwatson.com>
2015-11-25 16:44:34 -0700

    added easy API for using trained models

--- README.md
+++ README.md
@@ -36,3 +36,26 @@ An intermediate file with the suffix ".svm" is written during processing that co
 The file test1.csv is tiny: it just includes 16 examples for 2 classifications (or categries). For practical applications the more training data the better.
 
 About 30% of the input data is set aside for testing the trained model.
+
+## API for calling the model
+
+The following **irb** session shows how to cll the trained model:
+
+````````
+✗ irb
+2.2.1 :001 > load './liblinear_ruby_classifier.rb'
+
+gold: 0	prediction: 0	original data: EDUCATION	The students turned in their homework at school before summer break.
+gold: 1	prediction: 1	original data: HEALTH	The doctor and the nurse advised be to get rest because of my cold.
+gold: 1	prediction: 1	original data: HEALTH	The doctor had to go to the hospital.
+gold: 0	prediction: 0	original data: EDUCATION	The student lost her homework.
+gold: 1	prediction: 1	original data: HEALTH	The cold and flu season started this month.
+gold: 0	prediction: 1	original data: EDUCATION	The teacher yelled at the student who was late to class but later apologized.
+
+Percent correctly classified: 83
+ => true 
+2.2.1 :002 > SVMclassifier "the teacher said no homework for the student"
+ => "EDUCATION" 
+2.2.1 :004 > 
+````````
+
--- liblinear_ruby_classifier.rb
+++ liblinear_ruby_classifier.rb
@@ -11,8 +11,8 @@ temporary_feature_file = csv_input_file[0...-4] + '.svm'
 # default is tab delimited spreadsheet column data, to override:
 #::LibsvmPreprocessor::CSV_OPTIONS = {:col_sep=>",", :headers=>false}
 
-preprocessor = Preprocessor.new() # numeric_type: 2)
-preprocessor.use(csv_input_file, temporary_feature_file)
+$preprocessor = Preprocessor.new() # numeric_type: 2)
+$preprocessor.use(csv_input_file, temporary_feature_file)
 
 all_text = []
 File.open(csv_input_file).each do |line|
@@ -56,22 +56,31 @@ if false
   p "\n\n-----\n"
 end
 
-sp = LProblem.new(training_labels, training_maps, 1) # bias of 1
+$sp = LProblem.new(training_labels, training_maps, 1) # bias of 1
 pa = LParameter.new
 pa.solver_type = MCSVM_CS 
 pa.eps = 0.1
 
-model = LModel.new(sp, pa)
+$model = LModel.new($sp, pa)
 
 correct = 0
 
 for i in 0...testing_labels.size
   cls = testing_labels[i]
   maps = testing_maps[i]
-  prediction = model.predict(maps)
+  prediction = $model.predict(maps)
   puts "gold: #{cls}\tprediction: #{prediction}\toriginal data: #{testing_text[i]}"
   correct += 1 if cls == prediction
 end
 
 puts "\nPercent correctly classified: #{((correct.to_f / testing_labels.size) * 100).to_i}"
 
+$map_index_to_catagory = $preprocessor.categories.invert
+
+def SVMclassifier text
+  xs = text.split.collect {|x| $preprocessor.hash_of_ngrams[[x]] || -1}
+  xs = xs.select {|x| x > -1}
+  h = {}
+  xs.each {|x| h[x] = (h[x] || 0) + 1}
+  $map_index_to_catagory[$model.predict(h)]
+end
