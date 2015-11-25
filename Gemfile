a8d5298e8f8b8f228cfa033261895fa938f8577d
Mark Watson <markw@markwatson.com>
2015-11-25 09:54:24 -0700

    1. added Gemfile dependencies; added optional command line argument

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+
+ruby '2.2.1'
+
+gem 'tomz-liblinear-ruby-swig'
+gem 'libsvm_preprocessor'
+gem 'unicode'
--- liblinear_ruby_classifier.rb
+++ liblinear_ruby_classifier.rb
@@ -2,16 +2,20 @@ require "libsvm_preprocessor/preprocessor"
 require 'linear'
 require 'csv'
 
-# Input file: test1.csv. An intermediate file test1.svm (containing features) is created.
+# For an input file: test1.csv, then an intermediate file test1.svm (containing features) is created.
+
+csv_input_file = 'test1.csv'
+csv_input_file = ARGV[0] if ARGV.size > 0
+temporary_feature_file = csv_input_file[0...-4] + '.svm'
 
 # default is tab delimited spreadsheet column data, to override:
 #::LibsvmPreprocessor::CSV_OPTIONS = {:col_sep=>",", :headers=>false}
 
 preprocessor = Preprocessor.new() # numeric_type: 2)
-preprocessor.use("test1.csv", "test1.svm")
+preprocessor.use(csv_input_file, temporary_feature_file)
 
 all_text = []
-File.open('test1.csv').each do |line|
+File.open(csv_input_file).each do |line|
   all_text << line
 end
 
@@ -23,7 +27,7 @@ testing_labels = []
 testing_text = []
 
 count = 0
-File.open('test1.svm').each do |line|
+File.open(temporary_feature_file).each do |line|
   index = line.index(" ")
   cls = line[0..index].to_i
   text = line[index..-1].strip.split
